3f82699cf4f7525feeecc95719c46d002c0eaa81
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2023-05-18 16:30:16 -0400

    adds a check before renaming

--- functions.R
+++ functions.R
@@ -1,8 +1,32 @@
 ## functions
 
 
+## updated functions for this process
+
+#' Make a prior draws data frame
+#'
+#' @param brms_prior_model result of a call to brm with sample_prior = only
+#' @param draw_vec a vector of numbers to pull out the draws you want
+#' @param respname the quoted name of the response variable. This will be the
+#'   name of the prior predicted observations in the output
+#'
+#' @return returns a tibble with one row per simulation. For each simulation
+#'   ther eis a list-column, containing a data.frame with three columns:
+#' * the original data (same in every simulation)
+#' * the posterior epred, ie a simulation of the average. aka the deterministic part.
+#' * the posterior predictions (simulated observations, suitable for refitting the model)
 make_prior_draws_df <- function(brms_prior_model,
-                                draw_vec = 25:44){
+                                draw_vec = 25:44,
+                                respname = "y"){
+
+  ## rename data column
+
+  original_data <-   brms_prior_model$data
+
+  stopifnot(respname %in% names(original_data))
+
+  original_data_x <- original_data[-which(names(original_data) == respname)]
+
 
   brms_prior_model |>
     as.data.frame(draw = draw_vec) |>
@@ -11,7 +35,7 @@ make_prior_draws_df <- function(brms_prior_model,
     dplyr::mutate(
       simulated_data = list(dplyr::bind_cols(
         # extract model data
-        brms_prior_model$data,
+        original_data_x,
         # observations simulated from the prior
         y_new = brms::posterior_predict(
           brms_prior_model, draw_ids = draw_id) |> c(),
@@ -19,19 +43,9 @@ make_prior_draws_df <- function(brms_prior_model,
         epred = brms::posterior_epred(
           brms_prior_model, draw_ids = draw_id) |> c()
       ) |>
-        # most awkward part -- rename placeholder y variable
-        dplyr::select(-y) |>
-        dplyr::rename(y = y_new)
+        dplyr::rename(!!respname := y_new)
       )
     )
-}
-
-make_unnest_prior_dataframe <- function(prediction_df,
-                                        original_data,
-                                        x_name){
-  prediction_df |>
-    dplyr::mutate(num_obs = list(original_data[x_name])) |>
-    tidyr::unnest(cols = c(epred, preds, num_obs))
 }
 
 merge_prior_and_posterior_summary <- function(prior_df, posterior_df){
@@ -74,6 +88,19 @@ set_up_one_model <- function(one_sampled_model, dataframe){
 }
 
 
+#' Create a dataframe of prior simulations.
+#'
+#' @param data_values a dataframe representing 'fake data', ie the design. It
+#'   should look just like the data that the experiment will produce. it needs
+#'   to have a column for the response, but that column won't be used
+#' @param prior_spec a prior specification, ready to use in the model.
+#' @param bf_object the output of `bf`, specifying the model.
+#' @param draws_wanted the draws desired. Can be any sequence of numbers less that 1000
+#'
+#' @return This function runs the model simulations and returns a dataframe with
+#'   one row for each simulation. It performs two steps: first, prior
+#'   simulations from a brms model. Then it processes these into a dataframe with `make_prior_draws_df`
+#'
 simulate_from_prior <- function(data_values,
                                 prior_spec,
                                 bf_object,
